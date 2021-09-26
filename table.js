a469d1200fa0efb383e13a2379e8264e3675bc74
Ved Pradhan <vedprad1@gmail.com>
2021-09-25 18:11:26 -0700

    Transferred table initialization from table.js to index.html.

--- table.js
+++ table.js
@@ -1,8 +1,1 @@
-var table = document.getElementsByTagName('table');
-
-window.onload = function() {
-  table = '<tr><th>[Enter Column Name]</th>';
-  table += '<th>+Column</th></tr>';
-  table += '<tr><th>[Enter Value]</th></tr>';
-  table += '<tr><th>+Row</th></tr>';
-}
+var table = document.getElementsByTagName('table')[0];
