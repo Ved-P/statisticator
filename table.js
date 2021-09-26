0554d81a7fa73fd88ef6563d1ff3ee2ac03a4b2b
Ved Pradhan <vedprad1@gmail.com>
2021-09-25 17:48:53 -0700

    Added function to initialize the table.

---
+++ table.js
@@ -0,0 +1,8 @@
+var table = document.getElementsByTagName('table');
+
+function initTable() {
+  table = '<tr><th>[Enter Column Name]</th>';
+  table += '<th>+Column</th></tr>';
+  table += '<tr><th>[Enter Value]</th></tr>';
+  table += '<tr><th>+Row</th></tr>';
+}
